1eb53e352686fedf9673b9317e47040e1a836886
sokol gora <sokolgora@gmail.com>
2025-11-15 19:08:39 +0000

    1) Install test deps npm i -D jest ts-jest @types/jest
    2) Add Jest conf

---
+++ jest.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('jest').Config} */
+export default {
+  preset: 'ts-jest/presets/default-esm',
+  testEnvironment: 'node',
+  extensionsToTreatAsEsm: ['.ts'],
+  globals: { 'ts-jest': { useESM: true } },
+  roots: ['<rootDir>/tests'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^(\\.{1,2}/.*)\\.js$': '$1'
+  }
+};
