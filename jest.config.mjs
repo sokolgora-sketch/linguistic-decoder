5247f3522c78539988d8f216b45c3b52a3cca309
sokol gora <sokolgora@gmail.com>
2025-11-16 05:21:09 +0000

    C) (Optional) Silence the ts-jest warning (later)
    Not urgent, but if yo

--- jest.config.mjs
+++ jest.config.mjs
@@ -3,7 +3,9 @@ export default {
   preset: 'ts-jest/presets/default-esm',
   testEnvironment: 'node',
   extensionsToTreatAsEsm: ['.ts'],
-  globals: { 'ts-jest': { useESM: true } },
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', { useESM: true }],
+  },
   roots: ['<rootDir>/tests'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
