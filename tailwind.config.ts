7f5d2c83572cf0a77a8b3b732940cf40ce232270
sokol gora <sokolgora@gmail.com>
2025-11-17 17:54:08 +0000

    do these steps in order But there’s one small bug in your tailwind.confi

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,7 +5,7 @@ export default {
   darkMode: ['class'],
   content: [
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,tsjsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/lib/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
