dce11495a23e686596b827b7a1ad52e92c6bd883
sokol gora <sokolgora@gmail.com>
2025-11-22 17:18:55 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve './

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import "./globals.css";
+import "@/app/globals.css";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import { Toaster } from "@/components/ui/toaster";
 import { Inter } from "next/font/google";
