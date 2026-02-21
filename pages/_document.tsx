7a516181eceb43b16c09bdd65b72b5682b049f94
Sokol Gora <sokolgora@gmail.com>
2026-02-21 13:42:07 +0800

    fix(build): restore pages router build stability (_document + tsconfig include)  (#514)
    * fix(next): add pages/_document and 404 to unblock prerender build
    
    * fix(build): include pages in tsconfig + keep jsx preserve

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,16 +1,13 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from "next/document";
 
-// Explicit Document file to avoid Next build/runtime "Cannot find module for page: /_document".
-export default class MyDocument extends Document {
-  render() {
-    return (
-      <Html>
-        <Head />
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
